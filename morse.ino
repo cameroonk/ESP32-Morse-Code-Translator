ca98644afc70f7e42426c7a349adc0336ca6654e
cameroonk <cameron.james.kaz@gmail.com>
2025-05-21 12:20:25 -0500

    Print on Push

--- morse.ino
+++ morse.ino
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #define LED 2
-#define DOT_BUTTON 12
-#define DASH_BUTTON 26
+#define DOT_BUTTON 12 // WRONG, I will have to find something else to be the second input b/c the EN button I was using actually just resets the ESP OOPS
+#define DASH_BUTTON 0 // CORRECT
 
 // GLOBAL VARIABLES 
 int buttonState = 5; 
@@ -16,45 +16,55 @@ void setup() {
   pinMode(DOT_BUTTON, INPUT);
   pinMode(DASH_BUTTON, INPUT);
 	pinMode(LED, OUTPUT);
+
+	Serial.begin(9600);
+	delay(500); 
 }
 
 
 void loop() {
 //	buttonState = digitalRead(DOT_BUTTON);
-	/* Serial.begin(9600); */
+
 	int iterations = 0;
 	
 	struct link *curr_link = head;
 	while(iterations < 100000000000) {
-		//buttonState = digitalRead(pushButton);	
 		curr_link->next=NULL;	// this will make sure that if we do not read any input the evaluation will automatically terminate after this node
 				
-			delay(200);
-		
 		//read input
 		//
 		//if short add left
-		if(digitalRead(DOT_BUTTON == LOW)) {
+		if(digitalRead(DOT_BUTTON) == HIGH && 0) {
 			curr_link->data = DOT;
 			curr_link->next = (struct link*)malloc(sizeof(struct link));	// add another node to the linked list to add direction to next cycle
       curr_link = curr_link->next;
+			
 			digitalWrite(LED,HIGH);		
+			
+			delay(200);
+			Serial.print("DOT->");
+			iterations = 0;	
 			continue;
 		}
 		//if long add right	
-		if(digitalRead(DOT_BUTTON == LOW)) {
+		if(digitalRead(DASH_BUTTON) == LOW) {
 			curr_link->data = DASH;
 			curr_link->next = (struct link*)malloc(sizeof(struct link)); 
 			curr_link = curr_link->next;
+			
 			digitalWrite(LED,LOW);		
+
+			delay(200);
+			Serial.print("DASH->");
+			iterations = 0;	
 			continue;	
 		}
 
 		//MAKE SURE TO MAKE LAST NODE->NEXT NULL TO AVOID INFINITE LOOP		
-	
 		iterations++;
 	}
+	Serial.println("END\n");	
 	int result = evaluate(head);
-	printf("%c...", alphabet[result]);
-	free(head);
+//	Serial.println("%c...", alphabet[result]);
+	free(head);	// wonderful
 }
